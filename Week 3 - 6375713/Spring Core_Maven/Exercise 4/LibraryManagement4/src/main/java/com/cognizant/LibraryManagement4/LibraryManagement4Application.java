d886c14e5684de02c9d190211a46168f233e39f4
Anshika1321 <www.anshikaaneja@gmail.com>
2025-07-06 01:10:40 +0530

    Adding Week 3 Hands-on
    Spring Core and Maven & Spring Data JPA with Spring Boot, Hibernate

---
+++ Week 3 - 6375713/Spring Core_Maven/Exercise 4/LibraryManagement4/src/main/java/com/cognizant/LibraryManagement4/LibraryManagement4Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.LibraryManagement4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagement4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagement4Application.class, args);
+	}
+
+}
