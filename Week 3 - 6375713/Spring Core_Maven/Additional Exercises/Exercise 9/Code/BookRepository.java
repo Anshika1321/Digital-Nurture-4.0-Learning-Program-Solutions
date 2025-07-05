7eb0e8d58e85255d38ffec9b98dfbbf7dbb63d2e
Anshika1321 <www.anshikaaneja@gmail.com>
2025-07-05 21:30:37 +0530

    Delete Week 3 - 6375713/Spring Core_Maven directory

--- Week 3 - 6375713/Spring Core_Maven/Additional Exercises/Exercise 9/Code/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.LibraryManagement9.repository;
-
-import com.cognizant.LibraryManagement9.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
