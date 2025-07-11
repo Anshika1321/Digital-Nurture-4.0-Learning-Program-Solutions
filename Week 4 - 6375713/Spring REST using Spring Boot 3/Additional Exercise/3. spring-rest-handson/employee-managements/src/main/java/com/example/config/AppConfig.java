8ce64b9520338bcbdd813ed8b4d983cc1f214c79
Anshika1321 <www.anshikaaneja@gmail.com>
2025-07-11 21:13:42 +0530

    Delete Week 4 - 6375713 directory

--- Week 4 - 6375713/Spring REST using Spring Boot 3/Additional Exercise/3. spring-rest-handson/employee-managements/src/main/java/com/example/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // XML import removed to avoid duplicate loading
-}
