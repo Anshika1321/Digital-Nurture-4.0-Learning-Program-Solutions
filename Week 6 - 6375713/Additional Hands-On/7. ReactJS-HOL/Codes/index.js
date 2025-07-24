099dfde6c6c33f4fe539f8fb8181f0781f36a0d6
Anshika1321 <www.anshikaaneja@gmail.com>
2025-07-24 21:41:08 +0530

    Delete Week 6 - 6375713 directory

--- Week 6 - 6375713/Additional Hands-On/7. ReactJS-HOL/Codes/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import OnlineShopping from './OnlineShopping';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<OnlineShopping />);
